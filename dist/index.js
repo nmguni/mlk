0aec5dc1d03289785568dd6403f9b5561514b334
Ndumiso Mguni <Ndumiso29@gmail.com>
2020-01-14 11:39:01 -0500

    content added to about section

--- dist/index.js
+++ dist/index.js
@@ -184,36 +184,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"styles/main.scss":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"./..\\images\\martin-l.png":[["martin-l.8a8e00f0.png","images/martin-l.png"],"images/martin-l.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
-"use strict";
-
-require("./styles/main.scss");
-
-// tabs
-var tabLinks = document.querySelectorAll(".tablinks");
-var tabContent = document.querySelectorAll(".tabcontent");
-tabLinks.forEach(function (el) {
-  el.addEventListener("click", openTabs);
-});
-
-function openTabs(el) {
-  var btnTarget = el.currentTarget;
-  var country = btnTarget.dataset.country;
-  tabContent.forEach(function (el) {
-    el.classList.remove("active");
-  });
-  tabLinks.forEach(function (el) {
-    el.classList.remove("active");
-  });
-  document.querySelector("#" + country).classList.add("active");
-  btnTarget.classList.add("active");
-}
-},{"./styles/main.scss":"styles/main.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -417,5 +388,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/src.e31bb0bc.js.map
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+//# sourceMappingURL=/index.js.map
