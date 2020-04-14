454f74aa1b51996188fb20b68281b22e61abdae1
Ndumiso Mguni <Ndumiso29@gmail.com>
2020-04-13 20:27:14 -0400

    mobile complete

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\images\\March on Washington3.jpg":[["March on Washington3.0942970a.jpg","images/March on Washington3.jpg"],"images/March on Washington3.jpg"],"./..\\images\\martin-luther-speech.jpg":[["martin-luther-speech.5909f400.jpg","images/martin-luther-speech.jpg"],"images/martin-luther-speech.jpg"],"./..\\images\\martin-luther-flag.jpg":[["martin-luther-flag.aa514a24.jpg","images/martin-luther-flag.jpg"],"images/martin-luther-flag.jpg"],"./..\\images\\martin-luther-king-jr.jpg":[["martin-luther-king-jr.02a90d5e.jpg","images/martin-luther-king-jr.jpg"],"images/martin-luther-king-jr.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
+},{"./..\\images\\March on Washington3.jpg":[["March on Washington3.0942970a.jpg","images/March on Washington3.jpg"],"images/March on Washington3.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
 require("./styles/main.scss");
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51286" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49987" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
